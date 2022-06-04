5b43448619d2938fbd7fc05cab146116bdc1a648
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-06-04 20:57:43 +0200

    first Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Ahmed Amin on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
