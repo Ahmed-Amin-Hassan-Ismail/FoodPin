792f1694080d53f4d8cbf5dba3194becdb1deb24
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-06-14 20:26:23 +0200

    create object model and object class manually

--- FoodPin/Model/Restaurant.swift
+++ FoodPin/Model/Restaurant.swift
@@ -6,27 +6,39 @@
 //
 
 import Foundation
+import CoreData
 import Combine
 
-class Restaurant: ObservableObject {
-    @Published var name: String
-    @Published var type: String
-    @Published var location: String
-    @Published var phone: String
-    @Published var description: String
-    @Published var image: String
-    @Published var isFavorite: Bool
-    @Published var rating: Rating?
+public class Restaurant: NSManagedObject {
     
-    init(name: String, type: String, location: String, phone: String, description: String, image: String, isFavorite: Bool = false, rating: Rating? = nil) {
+    @NSManaged public var name: String
+    @NSManaged public var type: String
+    @NSManaged public var location: String
+    @NSManaged public var phone: String
+    @NSManaged public var summary: String
+    @NSManaged public var image: Data
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var ratingText: String?
+    
+    init(name: String, type: String, location: String, phone: String, summary: String, image: Data, isFavorite: Bool = false, ratingText: String? = nil) {
         
         self.name = name
         self.type = type
         self.location = location
         self.phone = phone
-        self.description = description
+        self.summary = summary
         self.image = image
         self.isFavorite = isFavorite
-        self.rating = rating
+        self.ratingText = ratingText
+    }
+    
+    var rating: Rating? {
+        get {
+            guard let ratingText = ratingText else { return nil }
+            return Rating(rawValue: ratingText)
+        }
+        set {
+            self.ratingText = newValue?.rawValue
+        }
     }
 }
